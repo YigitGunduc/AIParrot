c4c673e24e44a84d662c17321f6b1f0ff794daea
Yigit <yy347112@gmail.com>
2021-02-18 13:22:41 +0300

    initial commit for webapp

---
+++ webapp/app.py
@@ -0,0 +1,23 @@
+import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(predict.create_response(str(userText)))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
